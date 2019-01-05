bd1e8702a9ec26c833d352a7b5005dba03572ae0
Maciej Kubiak <maciej.kubiak.net@gmail.com>
2019-01-05 18:49:26 +0100

    Adjust config

--- webpack.prod.js
+++ webpack.prod.js
@@ -52,7 +52,7 @@ module.exports = {
       {
         test: /\.scss$/,
         use: [
-            "style-loader",
+            MiniCssExtractPlugin.loader,
             "css-loader",
             "sass-loader"
         ]
